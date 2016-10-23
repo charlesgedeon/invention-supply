cd0286bf64a3c58ad7c25bd80b88cf568308fa1d
Bjørn Karmann <bjoern.t.karmann@gmail.com>
2016-10-23 21:55:53 +0200

    invent botton

---
+++ json_converter/json_converter.pde
@@ -0,0 +1,38 @@
+
+JSONArray values;
+int id_count;
+
+void setup() {
+  
+  values = new JSONArray();
+  
+  for (int n=1; n<2000; n++) {
+    String lines[] = loadStrings("data/inventions"+n+".txt");
+    String collection = join(lines, "");
+    collection = collection + "## ";
+    
+    String[] split = split(collection, "##");
+
+    for (int i=0; i<split.length-1; i++) {
+  
+      if (split[i].length() > 2 && split[i].length() < 40) {
+        if (split[i+1].length() > 20) {  
+          id_count++;
+          println(id_count);     
+         
+          JSONObject json = new JSONObject();
+          
+          json = new JSONObject();
+          json.setInt("id",id_count);
+          json.setString("head", split[i]);
+          json.setString("text", split[i+1]);
+          json.setString("meening", "");
+          
+          values.setJSONObject(id_count, json);
+        }
+      }
+    }
+     saveJSONArray(values, "json/invention"+n+".json");
+  }
+ 
+}
--- sketch.js
+++ sketch.js
@@ -5,44 +5,76 @@ var display;
 
 var head;
 var text;
+var stat;
+var load = 0;
+var fade = 0;
+var time = 0;
+var click = false;
 
 function setup() {
-	loadJSON("data/inventions_small.json", drawData);
+	
 }
+
 
 function draw() {
 
-
+	var invent_button = document.getElementById("invent_button");
+	invent_button.addEventListener("click", doSomething);
+	var display = document.getElementById("display_invention");
+	var loading = document.getElementById("display_loading");
+	
+	if(click){
+		
+		console.log(load);
+		loading.style.display = "block";
+		loading.style.opacity = "1";
+		
+		if(load > time){	
+				
+			if(fade > 20){						
+				click = false;
+				load = 0;
+			}	
+			loading.style.opacity = "0";
+			display.style.display = "block";
+			display.style.opacity = fade*0.05;
+			fade ++;
+		}else{
+			display.style.display = "none";
+			display.style.opacity = "0";
+			fade = 0;
+			loadJSON("data/inventions_small.json", drawData);
+		}
+		load++;
+	}
 }
+
+function doSomething() {
+	time = random(100,400);
+	click = true;	
+}
+
+	
 
 
 
 function drawData(data){
 	
-	var number = int(random(33));
+	var h_head = document.getElementById("head");
+	var h_text = document.getElementById("text");
+	var p_stat = document.getElementById("stat");
 	
+	
+	var number = int(random(33));	
 	var invention = data[number];
 	head = invention["head"];
 	text = invention["text"];
+	stat = invention["meening"];
 	
-	display = createP(head);	
-	display.position(0,100);
-	display.style("font-family: 'Merriweather', serif");
-	display.style("font-weight: bold");
-	display.style("padding", "100px");
-	display.style("font-size", "24pt");
+	h_head.innerHTML = head;
+	h_text.innerHTML = text;
 
-
-	
-	display = createP(text);
-	display.position(0,180);
-	display.style("font-family: 'Open Sans', sans-serif");
-	display.style("width", "500px");
-	display.style("padding", "100px");
-	display.style("font-size", "14pt");
-	
-	
-	
+	p_stat.innerHTML = stat;
 }
 
 
